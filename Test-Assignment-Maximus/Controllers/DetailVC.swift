3c2fa785d4f6fc8c4eae7e70c2c7f2f33b84f27c
alexmosunov <alexmosunov@gmail.com>
2020-10-12 18:44:56 +0300

    version 1.13

--- Test-Assignment-Maximus/Controllers/DetailVC.swift
+++ Test-Assignment-Maximus/Controllers/DetailVC.swift
@@ -24,10 +24,14 @@ class DetailVC: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var watchButton: UIButton!
+    @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var gestureView: UIView!
     @IBOutlet weak var wallpaperImageTwo: UIImageView!
     @IBOutlet weak var wallpaperImageOne: UIImageView!
+    @IBOutlet weak var presetImageTwo: UIImageView!
+    @IBOutlet weak var presetImageOne: UIImageView!
+    
     
     
     var apiManager: APIManager?
@@ -39,6 +43,7 @@ class DetailVC: UIViewController {
     var wallpaperObject: DataModel?
     
     var numberOfTaps = 0
+    var previewImageIsBeingShown = false
     
     var spinnerView: UIView?
     
@@ -65,11 +70,14 @@ class DetailVC: UIViewController {
         gestureView.alpha = 0
         wallpaperImageOne.alpha = 0
         wallpaperImageTwo.alpha = 0
+        presetImageOne.alpha = 0
+        presetImageTwo.alpha = 0
 
         authorLabel.alpha = 0.0
         UIView.animate(withDuration: 0.7) {
             self.authorLabel.alpha = 1.0
         }
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -85,6 +93,7 @@ class DetailVC: UIViewController {
     
     func setUI() {
         watchButton.layer.cornerRadius = watchButton.frame.height / 2
+        downloadButton.layer.cornerRadius = downloadButton.frame.height / 2
         cancelButton.layer.cornerRadius = 50
         let blur = UIVisualEffectView(effect: UIBlurEffect(style:
             UIBlurEffect.Style.regular))
@@ -104,38 +113,40 @@ class DetailVC: UIViewController {
         if let apiManager = apiManager, let wallpaperObject = apiManager.wallpaperObject {
             print("wallpaperObject.image_1 - \(wallpaperObject.image_1)")
             guard let urlOne = URL(string: "https://pair.maximusapps.top/storage/\(wallpaperObject.image_1)") else {return}
-//            DispatchQueue.global().async {
-//                        if let data = try? Data(contentsOf: urlOne) {
-//                            if let image = UIImage(data: data) {
-//                                DispatchQueue.main.async {
-//                                    self.wallpaperImageOne.image = image
-//                                    self.activityIndicator(isEnabled: false)
-//                                }
-//                            }
-//                        }
-//                    }
-        
 
             wallpaperImageOne.kf.indicatorType = .activity
-            wallpaperImageOne.kf.setImage(with: urlOne)
+            wallpaperImageOne.kf.setImage(with: urlOne, completionHandler:  { result in
+                print(result)
+                self.presetImageOne.image = UIImage(named: "preset1")
+            })
+            
             
             guard let urlTwo = URL(string: "https://pair.maximusapps.top/storage/\(wallpaperObject.image_2)") else {return}
-            wallpaperImageTwo.kf.setImage(with: urlTwo)
             
+            wallpaperImageTwo.kf.setImage(with: urlTwo, completionHandler:  { result in
+                
+                self.presetImageTwo.contentMode = .top
+                self.presetImageTwo.clipsToBounds = true
+                let image = UIImage(named: "preset2")
+                
+                self.presetImageTwo.image = image?.resizeTopAlignedToFill(newWidth: self.imageView.frame.width)
+
+            })
+
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImage(sender:)))
             gestureView.addGestureRecognizer(tapGesture)
-//            activityIndicator(isEnabled: false)
         }
     }
     
     
     @IBAction func watchButtonTapped(_ sender: UIButton) {
-//        activityIndicator(isEnabled: true)
-        
+        previewImageIsBeingShown = true
         setPreviewImages()
         gestureView.alpha = 1.0
         wallpaperImageOne.alpha = 1.0
         wallpaperImageTwo.alpha = 1.0
+        presetImageOne.alpha = 1.0
+        presetImageTwo.alpha = 1.0
         
 
     }
@@ -149,6 +160,7 @@ class DetailVC: UIViewController {
         self.dismiss(animated: true)
     }
     
+
     
     
     @objc func handleTapOnImage(sender: UITapGestureRecognizer) {
@@ -160,35 +172,23 @@ class DetailVC: UIViewController {
             print(111)
             UIView.animate(withDuration: 0.2) {
                 self.wallpaperImageOne.alpha = 0.0
+                self.presetImageOne.alpha = 0.0
             }
         case 2:
             print(222)
             UIView.animate(withDuration: 0.2) {
                 self.wallpaperImageTwo.alpha = 0.0
+                self.presetImageTwo.alpha = 0.0
             }
             numberOfTaps = 0
             gestureView.alpha = 0.0
         default:
+            previewImageIsBeingShown = false
+            wallpaperImageOne.image = UIImage()
+            wallpaperImageTwo.image = UIImage()
             print("error with preview images")
         }
 
-    }
-    
-    func activityIndicator(isEnabled: Bool) {
-        if isEnabled {
-            spinnerView = UIView(frame: self.view.bounds)
-            spinnerView?.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-            let ai = UIActivityIndicatorView(style: .large)
-            ai.center = spinnerView!.center
-            spinnerView?.addSubview(ai)
-            self.view.addSubview(spinnerView!)
-            ai.startAnimating()
-        } else {
-            spinnerView?.removeFromSuperview()
-            spinnerView = nil
-        }
-        
-        
     }
     
 
@@ -206,6 +206,11 @@ extension DetailVC: UIScrollViewDelegate {
         if scrollView.contentOffset.y > 5 {
             let percent = scrollView.contentOffset.y / 100
             cancelButton.alpha = 1.0 - percent
+        }
+        
+        print(previewImageIsBeingShown)
+        if previewImageIsBeingShown {
+            contentScrollView.isScrollEnabled = false
         }
                 
     }
@@ -229,4 +234,22 @@ extension DetailVC: CardDetailViewController {
         return headerView
     }
 
+}
+
+
+
+
+extension UIImage {
+    func resizeTopAlignedToFill(newWidth: CGFloat) -> UIImage? {
+        let newHeight = size.height * newWidth / size.width
+
+        let newSize = CGSize(width: newWidth, height: newHeight)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, UIScreen.main.scale)
+        draw(in: CGRect(origin: .zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
 }
